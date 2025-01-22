73b6d21a40a72b5ec65890bfb823dc5ccb61b7c2
Theodore Coley <theodorecoley659@gmail.com>
2025-01-22 15:04:30 -0800

    Finished putting things together

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, abort, redirect
 
 import sys
 import sqlite3
@@ -31,12 +31,12 @@ def student():
         Lastname= request.args['lname']
         connection = sqlite3.connect(current_directory + "/sss.db")
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM Students WHERE Firstname=? AND Lastname=?", (Firstname,Lastname))
+        cursor.execute("SELECT * FROM Student WHERE firstname=? AND lastname=?", (Firstname,Lastname))
         result = cursor.fetchone()
         if (result is None):
-            return render_template('error.html')
+            error = ":( Sorry I wasn't able to find your name in my database please try again"
+            return render_template('error.html',error=error)
         else:
-   # res = list(result)
             connection.commit()
             connection.close()
             return render_template('student.html', result=result)
@@ -50,17 +50,53 @@ def question():
         Lastname= request.args['lname']
         connection = sqlite3.connect(current_directory + "/sss.db")
         cursor = connection.cursor()
-        cursor.execute("SELECT ID FROM Students WHERE Firstname=? AND Lastname=?", (Firstname,Lastname))
+        cursor.execute("SELECT ID FROM Student WHERE firstname=? AND lastname=?", (Firstname,Lastname))
         result = cursor.fetchone()
         id=result[0]
-        cursor.execute("SELECT QuestionNumber from SecurityQuestion WHERE StudentID=?",(str(id)))
+        cursor.execute("SELECT question from SecurityQuestion WHERE studentID=?",(str(id)))
         result2 = cursor.fetchone()
+        connection.commit()
+        connection.close()
         if (result2 is None):
-            return render_template('question.html')
+            return render_template('question.html',id=str(id))
         else:
-            return render_template('questionAnswer.html',q=Questiondict[result2[0]])
-        
+            return render_template('questionAnswer.html',q=Questiondict[result2[0]],id=str(id))
+
+@app.route('/studentinfo', methods=['GET','POST'])
+def student_info():
+    if request.method =="POST":
+        securityQuestion = request.form['questions']
+        securityAnswer = request.form['answer']
+        studentID = int(request.form['student_id'])
+        connection = sqlite3.connect(current_directory + "/sss.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO SecurityQuestion (studentID,question,answer) VALUES (?,?,?)",(studentID,securityQuestion,securityAnswer))
+        cursor.execute("SELECT * from Student WHERE ID=?",(str(studentID)))
+        student = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        #more logic needed to grap student detail
+        return render_template('studentInfo.html',student=student)
     
+
+@app.route('/checkanswer', methods=['GET','POST'])
+def check_answer():
+    if request.method =="POST":
+        answer = request.form['answer']
+        student_id = request.form['student_id']
+        connection = sqlite3.connect(current_directory + "/sss.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT answer from SecurityQuestion WHERE studentID=?",(student_id))
+        inquiry = cursor.fetchone()
+        if (answer == inquiry[0]):
+            cursor.execute("SELECT * from Student WHERE ID=?",(student_id))
+            student = cursor.fetchone()
+            connection.commit()
+            connection.close()
+            return render_template('studentInfo.html',student=student)
+        else:
+            error = " :( Sorry that was not the correct answer you must start over again"
+            return render_template('error.html',error=error)
 
 if __name__ == "__main__":
     app.debug=True
